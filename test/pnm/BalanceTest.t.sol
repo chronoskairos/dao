be8b0e8075d198e7203b52704b14f29237b407a2
pvorona <vorona.pavel.nickolaevich@gmail.com>
2022-10-31 15:51:48 +0300

    25 invariants

--- test/pnm/BalanceTest.t.sol
+++ test/pnm/BalanceTest.t.sol
@@ -15,7 +15,7 @@ contract BalanceTest is VaultTest {
         deposit(getAgent(), 100000 wei);
     }
 
-    function invariant() external {
+    function invariant0() external {
         uint256 balance = address(vault).balance;
 
         // INVARAINT:
@@ -31,7 +31,391 @@ contract BalanceTest is VaultTest {
         );
     }
 
-    function invariantFailing() external {
-        revert();
+    function invariant1() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
     }
+
+    function invariant2() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant3() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant4() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant5() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant6() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant7() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant8() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant9() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant10() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant11() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant12() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant13() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant14() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant15() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant16() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant17() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant18() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant19() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant20() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant21() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant22() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant23() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    function invariant24() external {
+        uint256 balance = address(vault).balance;
+
+        // INVARAINT:
+        // The vault should always have at least 1 ether.
+        // Otherwise, User cannot get the fund back.
+        require(
+            balance >= 1 ether,
+            string(abi.encodePacked(
+                "[!!!] Invariant violation: vault is stolen (",
+                balance,
+                ")"
+            ))
+        );
+    }
+
+    // function invariantFailing() external {
+    //     revert();
+    // }
 }
